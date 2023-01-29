b61d90917388e2039c0733b596c0879d7a8dae8b
donnol <donnol@outlook.com>
2023-01-29 17:06:19 +0800

    server: add no route

--- cmd/dsviz/main.go
+++ cmd/dsviz/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"log"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	dir := "JavascriptVisualRelease"
 	server := gin.Default()
-	server.Static("/", "JavascriptVisualRelease")
+	server.NoRoute(gin.HandlerFunc(func(c *gin.Context) {
+		c.File(filepath.Join(dir, "Algorithms.html"))
+	}))
+	server.Static("/", dir)
 	if err := server.Run(":8899"); err != nil {
 		log.Fatal(err)
 	}
